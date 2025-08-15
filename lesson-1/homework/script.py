40e59efd772157d2d6b5ee560ddb05ff552dd665
davronMH <kushchiyevdavron@gmail.com>
2025-08-15 16:40:27 +0500

    Create script.py

--- lesson-1/homework/script.py
+++ lesson-1/homework/script.py
@@ -1,28 +1,29 @@
-txt = 'MsaatmiazD'
-txt[::2]
-txt = 'MsaatmiazD'
-txt[9::-2]
-txt = "I'm John. I am from London"
-txt[20::]
-txt = "Davron"
-txt[5::-1]
-mevalar = ["olma", "anor", "shaftoli", "o'rik", "gilos"]
-print(mevalar[2])
-sonlar1 = [1,2,3]
-sonlar2 = [4,5,6]
-birlashtirilgan = sonlar1+sonlar2
-print(birlashtirilgan)
-sonlar = [10,20,30,40,50]
-yangi_royxat = [sonlar[0], sonlar[len(sonlar)//2], sonlar[-1]]
-print(yangi_royxat)
-shaharlar = ["London", "New York", "Toshkent", "Paris", "Tokyo"]
-if "Paris" in shaharlar:
-    print("Paris ro'yxatda bor")
-else:
-    print("Paris ro'yxatda yo'q")    
-sonlar = [1, 2, 3]
-yangi_royxat = sonlar * 2
-print(yangi_royxat)
-sonlar = [10, 20, 30, 40, 50]
-sonlar[0], sonlar[-1] = sonlar[-1], sonlar[0]
-print(sonlar)
+##Kvadratning yuzi
+a=5
+S=a**2
+print(S)
+##Doiraning uzuligi
+d = 10
+pi = 3.14
+L = pi * d
+print(L)  
+##Ikkita son a va b berilgan. Ularning o‘rtacha qiymatini toping.
+a=10
+b=20
+c=(a+b)/2
+print(c)
+##Ikkita son a va b berilgan. Ularning yig‘indisi, ko‘paytmasi va har birining kvadratini toping.
+a=10
+b=20
+c=a+b
+print(c)
+##Ikkita son a va b berilgan. Ularning ko‘paytmasi toping.
+a=10
+b=20
+c=a*b
+print(c)
+##Ikkita son a va b berilgan. Ularning har birining kvadratini toping.
+a=10**2
+b=20**2
+
+print(a,b)
